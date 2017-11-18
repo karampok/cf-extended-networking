5029df735d9e5bf60764d20d702f856181b58783
konstantinos <konstantinos.karampogias@swisscom.com>
2017-11-18 22:03:03 +0100

    Deploy with cf-extended-networking

--- scripts/deploy-silk-lite.sh
+++ scripts/deploy-silk-lite.sh
@@ -10,14 +10,16 @@ deployment_dir="deployments/multi-lite"
 mkdir -p "$deployment_dir"
 
 
-STEMCELL_VERSION=$(bosh int ~/workspace/cf-deployment/cf-deployment.yml --path /stemcells/alias=default/version)
-bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=$STEMCELL_VERSION
-bosh -n update-cloud-config manifests/cloud-config-lite.yml
-bosh update-runtime-config -n ~/workspace/bosh-deployment/runtime-configs/dns.yml
+# STEMCELL_VERSION=$(bosh int ~/workspace/cf-deployment/cf-deployment.yml --path /stemcells/alias=default/version)
+# bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=$STEMCELL_VERSION
+# bosh -n update-cloud-config manifests/cloud-config-lite.yml
+# bosh update-runtime-config -n ~/workspace/bosh-deployment/runtime-configs/dns.yml
 bosh -n -d silk-lite deploy \
   --vars-store "$deployment_dir/vars.yml" \
   -l manifests/bosh-lite.vars.yml \
   -o manifests/ops/use-compiled-releases.yml \
   -o manifests/ops/dns-aliases.yml \
+  -o manifests/ops/enable-cf-extended-networking.ops.yml \
+  -o manifests/ops/local-release.ops.yml \
   "$@" \
   manifests/silk-lite.yml
