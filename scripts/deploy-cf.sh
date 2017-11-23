87ee5c974b8281f5b85d72b2aaa1c1f59cb774e5
konstantinos <konstantinos.karampogias@swisscom.com>
2017-11-23 13:06:07 +0100

    Change the layout

--- scripts/deploy-cf.sh
+++ scripts/deploy-cf.sh
@@ -6,24 +6,27 @@ if [ ! -d "$cf_deployment" ]; then
   exit 1
 fi
 
-director_dir=~/workspace/bosh-lite
-mkdir -p "$director_dir"
-deployment_dir="$director_dir/cf"
+deployment_dir="deployments/multi-lite"
 mkdir -p "$deployment_dir"
 
 STEMCELL_VERSION=$(bosh int ~/workspace/cf-deployment/cf-deployment.yml --path /stemcells/alias=default/version)
-bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=$STEMCELL_VERSION
+bosh ss --json|grep "$STEMCELL_VERSION" || bosh upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v="$STEMCELL_VERSION"
 bosh -n update-cloud-config "$cf_deployment"/iaas-support/bosh-lite/cloud-config.yml
-# bosh update-runtime-config -n ~//workspace/bosh-deployment/runtime-configs/dns.yml
-bosh -n -d cf deploy \
+bosh update-runtime-config -n manifests/empty-runtime.yml
+bosh -n -d cf deploy --no-redact \
+  --vars-store "$deployment_dir/cf-vars.yml" \
   -o "$cf_deployment"/operations/bosh-lite.yml \
   -o "$cf_deployment"/operations/use-compiled-releases.yml \
   -o "$cf_deployment"/operations/scale-to-one-az.yml \
   -o "$cf_deployment"/operations/experimental/use-bosh-dns.yml \
-  -o manifests/disable-cf-networking.ops.yml \
-  -o manifests/enable-cf-extended-networking.ops.yml \
-  -o manifests/local-cf-net-release.ops.yml \
-  --vars-store "$deployment_dir/cf-vars.yml" \
+  -o ops/disable-cf-networking.ops.yml \
+  -o ops/enable-cf-extended-networking.ops.yml \
+  -o ops/local-release.ops.yml \
   -v system_domain="bosh-lite.com" \
   "$@" \
   "$cf_deployment"/cf-deployment.yml
+
+
+  #-o ~/workspace/cf-deployment/operations/experimental/use-bosh-dns-for-containers.yml \
+  # -o ~/workspace/service-discovery-release/opsfiles/opsfile.yml \
+
